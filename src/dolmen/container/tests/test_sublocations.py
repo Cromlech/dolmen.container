ce220826e5470edf9d6edc159c90c82a45e72097
Souheil CHELFOUH <trollfot@gmail.com>
2011-05-02 20:55:22 +0200

    Extracted all the docstrings to readble tests, with comments and easier comprehension

---
+++ src/dolmen/container/tests/test_sublocations.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""Get the sublocations for a container.
+"""
+
+from zope.location.interfaces import ISublocations
+from zope.interface.verify import verifyObject, verifyClass
+from dolmen.container.contained import ContainerSublocations
+
+
+class MyContainer(object):
+
+    def __init__(self, **data):
+        self.data = data
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+
+def test_sublocations():
+    assert verifyClass(ISublocations, ContainerSublocations)
+    
+    container = MyContainer(x=1, y=2, z=42)
+    adapter = ContainerSublocations(container)
+    assert verifyObject(ISublocations, adapter)
+
+    sublocations = list(adapter.sublocations())
+    sublocations.sort()
+    assert sublocations == [1, 2, 42]
